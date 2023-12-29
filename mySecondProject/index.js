0a524177e4ec180ba9b6d5a52a8267a527f7e1c4
YagoLisboa <yago.lisboa@live.com>
2023-12-28 23:51:15 -0300

    Definindo variáveis e modelando a função central do desafio

--- mySecondProject/index.js
+++ mySecondProject/index.js
@@ -1,1 +1,13 @@
-console.log("Minha primeira linha de código!")
+//variáveis para armazenar o número de vitórias e derrotas, o saldo de vitórias e o nível:
+let numVitorias;
+let numDerrotas;
+let saldoVitorias;
+let nivel;
+
+//Chamada e definição da função para calcular o número de partidas ranqueadas:
+saldoRanqueadas(numVitorias, numDerrotas);
+
+function saldoRanqueadas(a, b){
+    saldoVitorias = a - b;
+    return saldoVitorias;
+}
