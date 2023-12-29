d1313066842ab2f6c549056ef720290975e208f8
YagoLisboa <yago.lisboa@live.com>
2023-12-29 00:15:10 -0300

    Modelando a estrutura de decisão para definir o nível do Herói

--- mySecondProject/index.js
+++ mySecondProject/index.js
@@ -11,3 +11,20 @@ function saldoRanqueadas(a, b){
     saldoVitorias = a - b;
     return saldoVitorias;
 }
+
+//estrutura de decisão para apresentar o nível do Herói:
+if(saldoVitorias<10){
+    nivel = "Ferro";
+} else if(saldoVitorias>=11 && saldoVitorias<=20){
+    nivel = "Bronze";
+} else if(saldoVitorias>=21 && saldoVitorias<=50){
+    nivel = "Prata";
+} else if(saldoVitorias>=51 && saldoVitorias<=80){
+    nivel = "Ouro";
+} else if(saldoVitorias>=81 && saldoVitorias<=90){
+    nivel = "Diamante";
+} else if(saldoVitorias>=91 && saldoVitorias<=100){
+    nivel = "Lendário";
+} else {
+    nivel = "Imortal";
+}
